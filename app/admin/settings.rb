57a0b0a00774c02e3605949b9ea582a93da9d206
Szymon Rut <rut.szymon@gmail.com>
2012-07-23 13:33:19 +0200

    Edit settings for ActiveAdmin

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -3,6 +3,10 @@ ActiveAdmin.register Setting do
     column :key do |setting|
       t "setting.#{setting.key}"
     end
+    column :value
+    column do |setting|
+      link_to "Edytuj", edit_admin_setting_path(setting)
+    end
   end
 
   form do |f|
