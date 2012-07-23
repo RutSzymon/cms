639d91c8ee958a0568331cad569d87ed71801566
Szymon Rut <rut.szymon@gmail.com>
2012-07-23 16:16:35 +0200

    Contact factory

--- test/factories.rb
+++ test/factories.rb
@@ -3,4 +3,11 @@ FactoryGirl.define do
     sequence(:title){|n| "Tytul #{n}"}
     sequence(:body){|n| "Wstep #{n}"}
   end
+
+  factory :contact do
+    name "Name"
+    sequence(:email){|n| "example#{n}@example.com"}
+    topic "Temat"
+    body "Tresc"
+  end
 end
--- test/factories/sites.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :site do
-    title "MyString"
-    body "MyText"
-    permalink "MyString"
-  end
-end
