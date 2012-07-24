39fb7918528ee024c06c108ec030d4ed542b325a
Szymon Rut <rut.szymon@gmail.com>
2012-07-24 12:57:22 +0200

    Site unit tests

--- test/factories.rb
+++ test/factories.rb
@@ -5,6 +5,11 @@ FactoryGirl.define do
     sequence(:body){|n| "Tresc #{n}"}
   end
 
+  factory :site do
+    sequence(:title){|n| "Tytul #{n}"}
+    sequence(:body){|n| "Tresc #{n}"}
+  end
+
   factory :contact do
     name "Name"
     sequence(:email){|n| "example#{n}@example.com"}
--- test/unit/site_test.rb
+++ test/unit/site_test.rb
@@ -1,7 +1,35 @@
+#encoding: utf-8
 require 'test_helper'
 
 class SiteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @site = FactoryGirl.build(:site, title: "Żółwi tytuł 2")
+  end
+
+  test "it should save valid object" do
+    assert @site.save
+  end
+
+  test "it shouldn't save site without title" do
+    @site.title = nil
+    assert @site.invalid?
+  end
+
+  test "it shouldn't save without body" do
+    @site.body = nil
+    assert @site.invalid?
+  end
+
+  test "permalink should be generated automatically" do
+    @site.save
+    assert_equal "zolwi-tytul-2", @site.permalink
+  end
+
+  test "permalink should remember history" do
+    # @site.title = "Żółwi tytuł 2"
+    @site.save
+    @site.title = "Żółwi tytuł 3"
+    @site.save
+    assert_equal @site, Site.find("zolwi-tytul-2")
+  end
 end
