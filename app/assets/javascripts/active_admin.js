e0d2201b8b5f461cb4d693302be20902d21b61c8
Szymon Rut <rut.szymon@gmail.com>
2012-07-26 12:06:00 +0200

    Show MarkItUp for ActiveAdmin

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -2,7 +2,6 @@
 //= require markitup/jquery.markitup
 //= require markitup/sets/html/set
 
-//= require active_admin/application
 $(document).ready(function() {
   $("textarea").markItUp(mySettings);
 });
