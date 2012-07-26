30d5e3eb356237bc6c22a14ad9aec612c43950d9
Szymon Rut <rut.szymon@gmail.com>
2012-07-26 13:32:00 +0200

    Contact info

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,7 +1,12 @@
 class Setting < ActiveRecord::Base
-  attr_accessible :key, :value
+  attr_accessible :value
 
   validates :key, presence: true, uniqueness: true
+  validates :value, presence: true, if: :required?
+
+  def required?
+    required == true
+  end
 
   def self.get(key)
     where(key: key).first.try(:value)
