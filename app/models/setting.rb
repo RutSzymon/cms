e2d9e51d190228de3844624412b8546a5f04e4af
Szymon Rut <rut.szymon@gmail.com>
2012-07-26 14:04:20 +0200

    Content_type and value validations

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,7 +2,19 @@ class Setting < ActiveRecord::Base
   attr_accessible :value
 
   validates :key, presence: true, uniqueness: true
+  validates :content_type, presence: true, inclusion: ["email", "phone", "string", "text"]
   validates :value, presence: true, if: :required?
+
+  before_save :check_value
+
+  def check_value
+    case content_type
+    when "phone"
+      phone_validation
+    when "email"
+      email_validation
+    end
+  end
 
   def required?
     required == true
@@ -10,5 +22,14 @@ class Setting < ActiveRecord::Base
 
   def self.get(key)
     where(key: key).first.try(:value)
+  end
+
+  private
+  def phone_validation
+    errors.add(:value, "To nie jest tel.") unless value.match(/(\+?)(\d+)/)
+  end
+
+  def email_validation
+    errors.add(:value, "asddsadas") unless value.match(/^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i)
   end
 end
