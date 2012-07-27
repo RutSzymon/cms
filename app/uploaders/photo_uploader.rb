59f1a0e4bdc483516db10591aed33e57115257e2
Szymon Rut <rut.szymon@gmail.com>
2012-07-27 15:53:39 +0200

    Show photo in gallery

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -37,7 +37,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [120, 130]
+    process :resize_to_fill => [130, 120]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
