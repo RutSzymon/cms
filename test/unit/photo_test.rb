cc8831f8270b2ee7155ee653eb42d50a9b6e4515
Szymon Rut <rut.szymon@gmail.com>
2012-07-27 12:55:55 +0200

    Photo unit tests

--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @photo = FactoryGirl.build(:photo)
+  end
+
+  test "it should save valid object" do
+    assert @photo.save
+  end
+
+  test "it shouldn't save without title" do
+    @photo.title = nil
+    assert @photo.invalid?
+  end
+
+  test "it should save without description" do
+    @photo.description = nil
+    assert @photo.save
+  end
+
+  test "it should belong to gallery" do
+    gallery = FactoryGirl.create(:gallery)
+    @photo.gallery = gallery
+    assert_equal gallery, @photo.gallery
+  end
 end
