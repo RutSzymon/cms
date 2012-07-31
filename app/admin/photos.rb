8d758efb18b4fdd19949ecfdc02c78b3b5d41d94
Szymon Rut <rut.szymon@gmail.com>
2012-07-31 14:54:14 +0200

    Make_default photo action

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -4,7 +4,13 @@ ActiveAdmin.register Photo do
     "".tap do |content|
       content << link_to(image_tag(photo.photo.url(:thumb)), admin_photo_path(photo))
       content << link_to("Edytuj", edit_admin_photo_path(photo), class: "photos")
-      content << link_to("Ustaw jako domyślne", edit_admin_photo_path(photo), class: "photos")
+      content << link_to("Ustaw jako domyślne", make_deafault_admin_photo_path(photo), class: "photos")
     end.html_safe
+  end
+
+  member_action :make_deafault, :method => :get do
+    @photo = Photo.find(params[:id])
+    @photo.gallery.update_attribute(:photo_id, @photo.id)
+    redirect_to admin_photos_path, notice: "Ustawiono jako domyślne"
   end
 end
