6c97f79a45311789a4e990d33563dc17344f4cb6
Szymon Rut <rut.szymon@gmail.com>
2012-07-31 14:18:18 +0200

    Gallery in ActiveAdmin

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -1,3 +1,10 @@
+#encoding: utf-8
 ActiveAdmin.register Photo do
-
+  index :as => :grid, :columns => 5 do |photo|
+    "".tap do |content|
+      content << link_to(image_tag(photo.photo.url(:thumb)), admin_photo_path(photo))
+      content << link_to("Edytuj", edit_admin_photo_path(photo), class: "photos")
+      content << link_to("Ustaw jako domyślne", edit_admin_photo_path(photo), class: "photos")
+    end.html_safe
+  end
 end
